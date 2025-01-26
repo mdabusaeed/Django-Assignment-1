86210f1bd55a5407c62bce4f9e24b6d5aadc12c3
Abu Saeed <abu.saeed.nicl@gmail.com>
2025-01-26 13:17:29 +0600

    updater render name in setting

--- myproject/settings.py
+++ myproject/settings.py
@@ -25,11 +25,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-9ab2w4)bm8fw+lxjr=k4hm9+nvq7f*o8cf^2*f+7!l6ym7a=!3'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['django-assignment-1-1.onrender.com', '127.0.0.1']
 
-CSRF_TRUSTED_ORIGINS = ['https://*.onrender.com', 'http://127.0.0.1:8000']
+
+CSRF_TRUSTED_ORIGINS = ['https://django-assignment-1-1.onrender.com', 'http://127.0.0.1:8000']
 
 
    
