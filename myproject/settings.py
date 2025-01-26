69112c2cb7a50df46617ff1515b0153ab0993cd4
Abu Saeed <abu.saeed.nicl@gmail.com>
2025-01-26 11:33:39 +0600

    update setting files and prepare for deployment

--- myproject/settings.py
+++ myproject/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-9ab2w4)bm8fw+lxjr=k4hm9+nvq7f*o8cf^2*f+7!l6ym7a=!3
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['your-app-name.onrender.com', '127.0.0.1']
+ALLOWED_HOSTS = ['my-django-app.onrender.com', '127.0.0.1']
 
 CSRF_TRUSTED_ORIGINS = ['https://*.onrender.com', 'http://127.0.0.1:8000']
 
