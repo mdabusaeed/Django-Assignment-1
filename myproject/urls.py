a112b8dd4e05dd81882c6dc35af11acfcad460e9
mdabusaeed <abu.saeed.nicl@gmail.com>
2025-01-26 19:00:21 +0600

    url change

--- myproject/urls.py
+++ myproject/urls.py
@@ -17,11 +17,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.urls import path, include
-from tasks import views
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('tasks/', include('tasks.urls')),
-    path('', views.home, name='home'),
     path('__debug__/', include('debug_toolbar.urls')),
 ]
